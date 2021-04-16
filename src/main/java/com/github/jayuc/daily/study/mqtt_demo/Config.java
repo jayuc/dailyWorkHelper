c8a091fceb55d80b7a0b0ef4a92c8985f3c14570
余杰 <402842327@qq.com>
2021-04-16 21:11:29 +0800

    mqtt调试

--- src/main/java/com/github/jayuc/daily/study/mqtt_demo/Config.java
+++ src/main/java/com/github/jayuc/daily/study/mqtt_demo/Config.java
@@ -9,9 +9,13 @@ public class Config {
     /**
      * 订阅标识
      */
-    public static final String MQTT_TOPIC = "/xm/sh/response/mqtt/#";
+//    public static final String MQTT_TOPIC = "/xm/sh/response/mqtt/#";
 
-    public static final String R_MQTT_TOPIC = "/xm/sh/request/mqtt/1320420010110561/1011/v1";
+    public static final String MQTT_TOPIC = "home/status/";
+
+//    public static final String R_MQTT_TOPIC = "/xm/sh/request/mqtt/1320420010110561/1011/v1";
+
+    public static final String R_MQTT_TOPIC = "test";
 
     public static final String userName = "admin";
     public static final String password = "public";
--- src/main/java/com/github/jayuc/daily/study/mqtt_demo/MqttServer.java
+++ src/main/java/com/github/jayuc/daily/study/mqtt_demo/MqttServer.java
@@ -30,6 +30,7 @@ public class MqttServer {
             message.setRetained(false);
             long n = System.currentTimeMillis();
             String m = "{\"header\":{\"timeStamp\":\"" + n + "\"},\"payload\":{\"devices\":[{\"nodeid\":\"000\",\"action\":\"command\",\"params\":\"0110111100000000\"}]}}";
+            m = "11111111";
             message.setPayload(m.getBytes());
             client.connect(options);
 
